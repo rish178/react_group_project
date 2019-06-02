754238d9c247880dc2c24309d41d4973e21ae4f9
inDiscover <latif002@gmail.com>
2019-06-02 18:00:33 +0530

    Profile Screen changes

--- src/screens/profile/Profile.js
+++ src/screens/profile/Profile.js
@@ -1,12 +1,137 @@
-import React, {Component} from 'react';
 
-class Profile extends Component{
+import React from 'react';
+import './profile.css';
+import Fab from '@material-ui/core/Fab';
+import Icon from '@material-ui/core/Icon';
+import Modal from '@material-ui/core/Modal';
+import Typography from '@material-ui/core/Typography';
+import FormControl from '@material-ui/core/FormControl';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Button from '@material-ui/core/Button';
+import ImagePosts from './image-posts';
 
-    render() {
+class ProfileInfo extends React.Component{
+
+    constructor(){
+        super();
+        this.state = {
+            openOrCloseModal : false,
+            fullNameRequired : 'dispNone',
+            username : "",
+            profile_pic :"",
+            fullName : "",
+            posts : 0,
+            follows : 0,
+            followed_by : 0,
+            hdnFullName : ""
+        }
+
+        this.profileInfoURL = 'https://api.instagram.com/v1/users/self/?access_token=8661035776.d0fcd39.39f63ab2f88d4f9c92b0862729ee2784';
+    }
+
+    openModalHandler = () => {
+        this.setState({
+            openOrCloseModal:true,
+            fullNameRequired:'dispNone'
+        });
+    }
+
+    closeModalHandler = () => {
+        this.setState({
+            openOrCloseModal:false
+        });
+    }
+
+    inputFullNameChangeHandler = (e) => {
+        this.setState({
+            hdnFullName:e.target.value
+        })
+    }
+
+    updateFullNameHandler = () => {
+        this.state.hdnFullName === "" ? this.setState({fullNameRequired:'dispBlock'}) : this.setState({fullName:this.state.hdnFullName, hdnFullName:"", openOrCloseModal:false});
+    }
+
+    componentWillMount() {
+        let _context = this;        
+        let xhrProfileInfo = new XMLHttpRequest();
+        xhrProfileInfo.addEventListener("readystatechange", function () {
+            if (this.readyState === 4) {
+                let profInfoData = JSON.parse(this.responseText);
+                _context.setState({
+                    username:profInfoData.data.username,
+                    profile_pic:profInfoData.data.profile_picture,
+                    fullName:profInfoData.data.full_name,
+                    posts:profInfoData.data.counts.media,
+                    follows:profInfoData.data.counts.follows,
+                    followed_by:profInfoData.data.counts.followed_by
+                });
+            }
+        });
+
+        xhrProfileInfo.open("GET", this.profileInfoURL);
+        xhrProfileInfo.send();
+    }
+    
+    render(){        
         return(
-            <div>Profile</div>
+            <div>
+            <div className='prof-main-container'>
+                <Modal open={this.state.openOrCloseModal}
+                    aria-labelledby="simple-modal-title"
+                    onClose={this.closeModalHandler}
+                >
+                    <div className='modal-size'>
+                        <Typography variant="h6" id="modal-title">
+                            Edit
+                        </Typography>
+                        <br />
+                        <FormControl required>
+                            <InputLabel htmlFor="fullName"><span className="blue">Full Name</span></InputLabel>
+                            <Input id="fullName" type="text" onChange={this.inputFullNameChangeHandler} />                            
+                            <FormHelperText className={this.state.fullNameRequired}>
+                                <span className="red">required</span>
+                            </FormHelperText>
+                        </FormControl>
+                        <br /><br /><br />
+                        <Button variant="contained" color="primary" onClick={this.updateFullNameHandler}>UPDATE</Button>
+                    </div>
+                </Modal>                
+                <div className='prof-items-container'>
+                    <div className='prof-pic-container'><img src={this.state.profile_pic} className="prof-pic" alt="profile pic" /></div>
+                    <div className='prof-info-data-container'>
+                        <div className='user-name'>{this.state.username}</div>
+                        <div className='posts-follows-container'>
+                            {/* <span className='posts-follows-item-first'>Posts:6</span>
+                            <span className='posts-follows-item'>Follows:4</span>
+                            <span className='posts-follows-item'>Followed By:6</span> */}
+                            <span>Posts: {this.state.posts}</span>
+                            <span className='posts-follows-item'>Follows: {this.state.follows}</span>
+                            <span>Followed By: {this.state.followed_by}</span>
+                        </div>
+                        <div className='full-name-container'>
+                            <div className='full-name'>{this.state.fullName}</div>
+                            <div className='full-name-edit-btn'>
+                            {/* <Button variant="fab" color="secondary" size='medium'>
+                                Edit
+                            </Button> */}
+                            <Fab color="secondary" aria-label="Edit" className='edit-btn' size='small' onClick={this.openModalHandler}>
+                                <Icon>edit_icon</Icon>
+                            </Fab>
+                            </div>
+                        </div>
+                    </div>
+                </div>                
+            </div>
+            <div>
+                <ImagePosts />
+            </div>
+            </div>
         )
     }
+   
 }
 
-export default Profile;
+export default ProfileInfo
---
+++ src/screens/profile/image-posts.js
@@ -0,0 +1,238 @@
+import React from 'react';
+import './image-posts.css';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import Modal from '@material-ui/core/Modal';
+import { withStyles } from '@material-ui/core/styles';
+import Divider from '@material-ui/core/Divider';
+import FavoriteIcon from "@material-ui/icons/FavoriteBorderOutlined";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from '@material-ui/core/Typography';
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Favorite from "@material-ui/icons/Favorite";
+
+const styles = theme => ({
+    root: {
+      flexGrow: 1,
+    },
+    image: {
+      width: '450px',
+      height: '450px',
+    },
+    img: {
+      margin: 'auto',
+      display: 'block',
+      maxWidth: '100%',
+      maxHeight: '100%',
+    },
+    textField: {
+        marginLeft: 0,
+        marginRight: 0,
+        width: 210
+    },
+    button: {
+        marginTop: 30,
+        width: 50,
+        marginLeft: 5
+    }
+  });
+
+class ImagePosts extends React.Component{
+
+    constructor(){
+        super();
+        this.state = {
+            imagePosts : [],
+            openOrCloseModal:false,
+            imageURL:'',
+            alt:'',
+            profilePicture:'',
+            userName:'',
+            caption:'',
+            hashTags:'',
+            likesCount:0,
+            likeIndicatorStatusFlag:0,
+            likeIndicatorStatusStyle:{},
+            comments:[],
+            currentComment:''
+        }
+        this.imageMediaURL = 'https://api.instagram.com/v1/users/self/media/recent?access_token=8661035776.d0fcd39.39f63ab2f88d4f9c92b0862729ee2784';
+    }
+
+    appendHashTag = (values) => {
+        let hashedTags = values.map(item => (
+            '#'+item
+        ));        
+        let hashedTagString = hashedTags.join(' ');
+        return hashedTagString;
+    }
+
+    removeHashTags = (value) => {
+        let cpationString = value.split("\n");
+        return cpationString[0];
+    }
+
+    likeIndicatorStatusChangeHandler = () => {
+        this.setState((prevState) => {            
+            if(prevState.likeIndicatorStatusFlag === 0){
+                return { likeIndicatorStatusFlag: 1,likeIndicatorStatusStyle :{ color: "#b20505" },likesCount:prevState.likesCount+1};
+            }else{
+                return { likeIndicatorStatusFlag: 0,likeIndicatorStatusStyle :{},likesCount:prevState.likesCount-1};
+            }
+        })
+    }
+
+    setCommentHandler = (cmnt) => {        
+        this.setState({currentComment:cmnt.target.value});
+    }
+
+    addCommentHandler = () => {        
+        this.setState({comments:this.state.comments.concat(this.state.currentComment)});
+    }
+
+    componentWillMount() {
+        let _context = this;        
+        let xhrImagePosts = new XMLHttpRequest();
+        xhrImagePosts.addEventListener("readystatechange", function () {
+            if (this.readyState === 4) {
+                let imageMediaData = JSON.parse(this.responseText);
+                var imagePostsData = imageMediaData.data.map(item => (
+                    {
+                        id:item.id
+                        ,imgURL: item.images.standard_resolution.url
+                        ,alt:item.caption.text
+                        ,profile_pic:item.user.profile_picture
+                        ,username:item.user.username
+                        ,caption:_context.removeHashTags(item.caption.text)
+                        ,hashtags:_context.appendHashTag(item.tags)
+                        ,likesCount:item.likes.count                        
+                    }
+                ));
+                _context.setState({
+                    imagePosts:imagePostsData
+                })                
+            }
+        });
+
+        xhrImagePosts.open("GET", this.imageMediaURL);
+        xhrImagePosts.send();
+    }
+
+    openModalHandler = (imgData) => {
+        this.setState({
+            openOrCloseModal:true,
+            imageURL:imgData.imgURL,
+            alt:imgData.alt,
+            profilePicture:imgData.profile_pic,
+            userName:imgData.username,
+            caption:imgData.caption,
+            hashTags:imgData.hashtags,
+            likesCount:imgData.likesCount,
+            likeIndicatorStatusFlag:0,
+            likeIndicatorStatusStyle:{},
+            comments:[]
+        });
+    }
+
+    closeModalHandler = () => {
+        this.setState({
+            openOrCloseModal:false
+        });
+    }
+
+        
+    render(){ 
+        const { classes } = this.props;       
+        return(
+            <div className='impo-main-container'>
+                <Modal open={this.state.openOrCloseModal}
+                    aria-labelledby="simple-modal-title"
+                    onClose={this.closeModalHandler}
+                >
+                    <div className='modal-size-img-post'>
+                    
+                        <div className='modal-img-post-main-cntnr'>
+                            <div className='modal-img-post'>
+                                <img className='modal-img-post-url' alt="complex" src={this.state.imageURL} />
+
+                            </div> 
+                            <div className='modal-img-post-info'>
+                                <div className='modal-img-post-user-info'>
+                                    <div><img className='modal-img-post-prof-pic' alt='profile_pic' src={this.state.profilePicture} /></div>
+                                    <div className='modal-img-post-uname'>{this.state.userName}</div>
+                                </div>
+                                <div className='modal-img-post-user-info-divider'>
+                                    <Divider />
+                                </div>
+                                <div className='modal-img-post-user-info-captions'>
+                                    {this.state.caption}
+                                </div>
+                                <div className='modal-img-post-user-info-hash-tags'>
+                                    {this.state.hashTags}
+                                </div>
+                                <br />
+                                <div className='modal-img-post-user-info-comments'>
+                                    {this.state.comments.map((comment,index) => (                                        
+                                        <div key={index}>
+                                            <span className='modal-commnts-uname'>{this.state.userName}: </span>
+                                            <span>{comment}</span>
+                                        </div>
+                                    ))}                                  
+                                </div>
+                                <div className='modal-img-post-likes-contnr'>
+                                    <div>
+                                        <IconButton
+                                            onClick={this.likeIndicatorStatusChangeHandler}
+                                            aria-label="Add to favorites"
+                                            key={1}
+                                            style={this.state.likeIndicatorStatusStyle}
+                                        >
+                                            <Favorite />
+                                        </IconButton>
+                                    </div>
+                                    <div className='modal-img-post-likes-counter'>
+                                        <Typography className={classes.test}>
+                                            {this.state.likesCount} likes
+                                            
+                                        </Typography>
+                                    </div>
+                                </div>
+                                <div className='modal-img-post-add-comments'>
+                                    <form className="add-comment-form">
+                                        <TextField
+                                            label="Add a comment"
+                                            className={classes.textField}
+                                            margin="normal"
+                                            onChange={this.setCommentHandler}
+                                            defaultValue=""
+                                        />                                
+                                        <Button
+                                            variant="contained"
+                                            color="primary"
+                                            className={classes.button}
+                                            onClick={this.addCommentHandler}
+                                        >
+                                        ADD
+                                        </Button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>    
+                    
+                    </div>
+                </Modal>
+                <GridList cols={3} >
+                    {this.state.imagePosts.map(imgPost => (
+                        <GridListTile key={imgPost.id}>
+                            <img src={imgPost.imgURL} className="movie-poster" alt={imgPost.alt} onClick={this.openModalHandler.bind(this,imgPost)} />                            
+                        </GridListTile>
+                    ))}
+                </GridList>
+            </div>
+        )
+    }
+   
+}
+
+export default withStyles(styles)(ImagePosts);
